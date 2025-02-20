dc6b0fab3463a520ae34b001b193d3b31b4678da
sr-githaka <sr.githaka@gmail.com>
2025-02-20 15:56:57 +0300

    feat(app/public/authentication): add Register and Reset pages
    Created `/public/authentication/register` and `/public/authentication/reset` pages, utilizing the Authentication component with respective `type` props.
    
    This completes the authentication flow, providing dedicated routes for user registration and password reset.

---
+++ src/app/public/authentication/register/page.tsx
@@ -0,0 +1,23 @@
+import { Header, Main } from '@components/layout';
+import { Button, NavItem } from '@components/shared';
+import { Authentication } from '@components/view';
+
+export default function Register() {
+    return (
+        <>
+            <Header>
+                <NavItem href='/public/authentication/login' name='Login' />
+                <NavItem
+                    href='/public/authentication/register'
+                    name='Register'
+                    active
+                />
+                <NavItem href='/public/authentication/reset' name='Reset' />
+                <Button href='/' name='Landing' />
+            </Header>
+            <Main>
+                <Authentication type='Register' />
+            </Main>
+        </>
+    );
+}
---
+++ src/app/public/authentication/reset/page.tsx
@@ -0,0 +1,26 @@
+import { Header, Main } from '@components/layout';
+import { Button, NavItem } from '@components/shared';
+import { Authentication } from '@components/view';
+
+export default function Login() {
+    return (
+        <>
+            <Header>
+                <NavItem href='/public/authentication/login' name='Login' />
+                <NavItem
+                    href='/public/authentication/register'
+                    name='Register'
+                />
+                <NavItem
+                    href='/public/authentication/reset'
+                    name='Reset'
+                    active
+                />
+                <Button href='/' name='Landing' />
+            </Header>
+            <Main>
+                <Authentication type='Reset' />
+            </Main>
+        </>
+    );
+}
