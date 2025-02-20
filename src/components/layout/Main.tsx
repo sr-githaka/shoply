8197015069f40c4de3139230c2638335881d592b
sr-githaka <sr.githaka@gmail.com>
2025-02-20 13:30:16 +0300

    feat(layout/main, styles): integrate Container and Footer for improved structure
    - Updated `Main` component to include `Container` and `Footer` for better layout organization.
    - Added `Container` styles to manage content alignment and spacing.
    - Styled `Footer` for proper positioning and spacing at the bottom.
    - Adjusted `Main` layout to center-align content and start from the top.
    
    These enhancements improve layout consistency and readability across the application.

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,5 +1,11 @@
 import { MainStyles } from '@styles/layout';
+import { Container, Footer } from '@components/layout';
 
 export default function Main() {
-    return <main className={[MainStyles.Main, 'layout'].join(' ')}></main>;
+    return (
+        <main className={[MainStyles.Main, 'layout'].join(' ')}>
+            <Container />
+            <Footer />
+        </main>
+    );
 }
