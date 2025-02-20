c5b70f235a6febf8de8576a5e81930a188cac68c
sr-githaka <sr.githaka@gmail.com>
2025-02-20 01:04:59 -0800

    feat(components/layout): integrate Brand component into Header
    Added the `Brand` component to the `Header`, displaying the brand name alongside the logo.
    
    This enhances the header's branding by ensuring a consistent visual identity across the application.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,10 @@
+import { Brand } from '@components/shared';
 import { HeaderStyles } from '@styles/layout';
 
 export default function Header() {
     return (
-        <header className={[HeaderStyles.Header, 'layout'].join(' ')}></header>
+        <header className={[HeaderStyles.Header, 'layout'].join(' ')}>
+            <Brand name='POS' />
+        </header>
     );
 }
