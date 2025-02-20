238077e5090bb4c16f61c3bde874f26bc85d1afc
sr-githaka <sr.githaka@gmail.com>
2025-02-20 13:49:32 +0300

    feat(components/layout): add toggle functionality to Nav
    Implemented state management in the Nav component to toggle the visibility of the Small navigation body.
    
    This improves usability on smaller screens by allowing users to expand or collapse the navigation menu.

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,12 +1,24 @@
+'use client';
+import { useState } from 'react';
 import { NavStyles } from '@styles/layout';
 
 export default function Nav({ children }: Layout.Nav.Props) {
+    const [state, setState] = useState(false);
+
+    const toggleState = () => {
+        setState(!state);
+    };
+
     return (
         <nav className={NavStyles.Nav}>
             <div className={NavStyles.Large}>{children}</div>
             <div className={NavStyles.Small}>
-                <div className={NavStyles.Head}></div>
-                <div className={NavStyles.Body}>{children}</div>
+                <div className={NavStyles.Head} onClick={toggleState}></div>
+                {state ? (
+                    <div className={NavStyles.Body}>{children}</div>
+                ) : (
+                    <></>
+                )}
             </div>
         </nav>
     );
