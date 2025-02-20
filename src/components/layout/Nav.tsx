87f5690f3cfcec86de548a5743cbd77d3b828da6
sr-githaka <sr.githaka@gmail.com>
2025-02-20 02:07:22 -0800

    feat(components/layout): enhance Nav component with responsive design
    Updated the `Nav` component to support both large and small screen layouts. Introduced `.Large` for desktop navigation and `.Small` for mobile navigation with structured `.Head` and `.Body` sections.
    
    This improves responsiveness, ensuring a better user experience across different screen sizes.

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,5 +1,13 @@
 import { NavStyles } from '@styles/layout';
 
 export default function Nav({ children }: Layout.Nav.Props) {
-    return <nav className={NavStyles.Nav}>{children}</nav>;
+    return (
+        <nav className={NavStyles.Nav}>
+            <div className={NavStyles.Large}>{children}</div>
+            <div className={NavStyles.Small}>
+                <div className={NavStyles.Head}></div>
+                <div className={NavStyles.Body}>{children}</div>
+            </div>
+        </nav>
+    );
 }
