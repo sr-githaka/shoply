3682754ec90da9ba3be4e154152dc60628d4a9f1
sr-githaka <sr.githaka@gmail.com>
2025-02-20 01:49:18 -0800

    feat(/): integrate navigation items into Header on landing page
    Added `NavItem` components to the `Header` in the landing page, providing links to "Landing" and "Docs" sections.
    
    This enhances navigation accessibility and improves user experience by making key sections easily reachable.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Header, Main } from '@components/layout';
+import { NavItem } from '@components/shared';
 
 export default function Landing() {
     return (
         <>
-            <Header />
+            <Header>
+                <NavItem href='/' name='Landing' />
+                <NavItem href='/docs' name='Docs' />
+            </Header>
             <Main />
         </>
     );
