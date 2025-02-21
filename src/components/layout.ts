89cc0815dd8a2024caf9f868f999dff0a0c7dd46
sr-githaka <sr.githaka@gmail.com>
2025-02-21 11:13:06 +0300

    Revert "feat(layout): add Public and Private layout components"
    This reverts commit 26a9ab74679ba189fac644667356fc8067918444.

--- src/components/layout.ts
+++ src/components/layout.ts
@@ -4,6 +4,9 @@ export { default as Footer } from './layout/Footer';
 export { default as Container } from './layout/Container';
 export { default as View } from './layout/View';
 export { default as Nav } from './layout/Nav';
+<<<<<<< HEAD
 export { default as Public } from './layout/Public';
 export { default as Private } from './layout/Private';
 export { default as Aside } from './layout/Aside';
+=======
+>>>>>>> parent of 26a9ab7 (feat(layout): add Public and Private layout components)
--- src/components/layout/Public.tsx
+++
@@ -1,9 +0,0 @@
-import { PublicStyles } from '@styles/layout';
-
-export default function Public({ children }: Layout.Public.Props) {
-    return (
-        <section id='public' className={PublicStyles.Public}>
-            {children}
-        </section>
-    );
-}
