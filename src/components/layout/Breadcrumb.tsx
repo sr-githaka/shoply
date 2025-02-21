17662cdc2723b3f9247f27a21e8b42242c1c6d68
sr-githaka <sr.githaka@gmail.com>
2025-02-21 12:11:31 +0300

    feat(layout): enhance Breadcrumb with icon and styling
    Added an `Icon` component to the `Breadcrumb` for improved navigation.
    The breadcrumb now includes a `menu.svg` button with styling for better UI/UX.
    
    Additionally:
    - Updated `Breadcrumb.module.css` to structure the component with flexbox.
    - Introduced `.Button` styles with padding, border-radius, and hover effects.
    - Applied `background-color: var(--primary-color)` to `body` in `global.css`.
    
    These changes refine the user experience and maintain visual consistency.

--- src/components/layout/Breadcrumb.tsx
+++ src/components/layout/Breadcrumb.tsx
@@ -1,9 +1,18 @@
+import { Icon } from '@components/shared';
 import { BreadcrumbStyles } from '@styles/layout';
 
 export default function Breadcrumb() {
     return (
-        <div
-            className={[BreadcrumbStyles.Breadcrumb, 'layout'].join(' ')}
-        ></div>
+        <div className={[BreadcrumbStyles.Breadcrumb, 'layout'].join(' ')}>
+            <div className={BreadcrumbStyles.Button}>
+                <Icon
+                    name='menu.svg'
+                    description='Navigation icon'
+                    width={18}
+                    height={9}
+                />
+            </div>
+            <p>Dashboard / Analytics</p>
+        </div>
     );
 }
