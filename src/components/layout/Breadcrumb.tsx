4a1c223df2cf735942c3b8fc4f2e9d6b8a8c484f
sr-githaka <sr.githaka@gmail.com>
2025-02-21 13:00:28 +0300

    feat(components/layout): add toggle functionality to Breadcrumb
    Integrated `useTrigger` hook to enable toggling of the breadcrumb component.
    Updated the icon to switch between 'menu.svg' and 'cancel.svg' based on state.
    
    This enhancement improves navigation usability by allowing users to expand
    or collapse the breadcrumb component dynamically.

--- src/components/layout/Breadcrumb.tsx
+++ src/components/layout/Breadcrumb.tsx
@@ -1,19 +1,27 @@
+'use client';
+
+import { useTrigger } from '@core/hooks';
 import { Icon } from '@components/shared';
 import { BreadcrumbStyles } from '@styles/layout';
 
 export default function Breadcrumb({ children }: Layout.Breadcrumb.Props) {
+    const { trigger, toggleTrigger } = useTrigger();
     return (
         <div className={[BreadcrumbStyles.Breadcrumb, 'layout'].join(' ')}>
-            <div className={BreadcrumbStyles.Button}>
+            <div className={BreadcrumbStyles.Button} onClick={toggleTrigger}>
                 <Icon
-                    name='menu.svg'
+                    name={trigger ? 'cancel.svg' : 'menu.svg'}
                     description='Navigation icon'
                     width={18}
                     height={9}
                 />
             </div>
             <p>Dashboard / </p>
-            <div className={BreadcrumbStyles.Body}>{children}</div>
+            {trigger ? (
+                <div className={BreadcrumbStyles.Body}>{children}</div>
+            ) : (
+                <></>
+            )}
         </div>
     );
 }
