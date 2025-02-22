24a5c6b6c129990fad1621b79328ba6219957ff7
sr-githaka <sr.githaka@gmail.com>
2025-02-22 16:59:31 +0300

    feat(/): add metadata and update login URL in Landing page
    Added metadata to the Landing page, including a title and description for better SEO and accessibility. Updated the login button URL to `/public/authentication/login` to reflect the correct path.
    
    These changes improve the page's metadata for search engines and ensure the login button directs users to the correct authentication route.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import { Header, Main } from '@components/layout';
 import { Button, NavItem } from '@components/shared';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'POS',
+    description:
+        'A fast and efficient online point-of-sale system for seamless transactions.',
+};
 
 export default function Landing() {
     return (
@@ -7,7 +14,7 @@ export default function Landing() {
             <Header>
                 <NavItem href='/' name='Landing' active />
                 <NavItem href='/docs' name='Docs' />
-                <Button href='/authentication/login' name='Account' />
+                <Button href='/public/authentication/login' name='Account' />
             </Header>
             <Main>
                 <></>
