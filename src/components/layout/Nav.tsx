34feca4fd85657a3d5fb1b966aa8d9fd5f353fd7
sr-githaka <sr.githaka@gmail.com>
2025-03-06 23:15:51 +0300

    feat(nav): integrate responsive navigation with NavAside
    Highlights:
    - Updated the Nav component to wrap its children in a default container and include a NavAside component.
    - Modified the NavAside component to render a menu icon in its head section using the Icon component.
    - Added a .Default CSS class in Nav.module.css to display the default navigation on larger screens and hide it on smaller screens, while configuring NavAside to appear on smaller screens via media queries.
    
    These changes enhance the navigation experience by providing a mobile-friendly sidebar (NavAside) with a menu icon for easier navigation on small screens, while maintaining a traditional navigation layout on larger devices.

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,6 +1,12 @@
 import { NavTypes } from '@_types/components/layout';
+import { NavAside } from '@components/shared';
 import { NavStyles } from '@styles/layout';
 
 export default function Nav({ children }: NavTypes.Props) {
-    return <nav className={NavStyles.Nav}>{children}</nav>;
+    return (
+        <nav className={NavStyles.Nav}>
+            <div className={NavStyles.Default}>{children}</div>
+            <NavAside>{children}</NavAside>
+        </nav>
+    );
 }
--- src/components/shared/NavAside.tsx
+++ src/components/shared/NavAside.tsx
@@ -1,10 +1,18 @@
 import { NavAsideTypes } from '@_types/components/shared';
 import { NavAsideStyles } from '@styles/shared';
+import { Icon } from '@components/shared';
 
 export default function NavAside({ children }: NavAsideTypes.Props) {
     return (
         <aside className={NavAsideStyles.NavAside}>
-            <div className={NavAsideStyles.Head}></div>
+            <div className={NavAsideStyles.Head}>
+                <Icon
+                    name='menu.svg'
+                    alt='Menu icon'
+                    width={24}
+                    height={24}
+                />
+            </div>
             <div className={NavAsideStyles.Body}>{children}</div>
         </aside>
     );
