6fd7249066aaed7efe448489f3d227a442c46294
sr-githaka <sr.githaka@gmail.com>
2025-03-06 21:57:21 +0300

    feat(layout): update header branding with logo and text
    Highlights:
    - Updated the Header component to include the Brand component instead of an empty header.
    - Modified the Brand component to render the Logo and display the "Volty" brand name.
    - Adjusted Header.module.css to align content to the start and added a border-bottom for visual separation.
    - Created and updated Brand.module.css to set up a flex layout with appropriate spacing and typography.
    - Updated global.css by changing the --highlight-color to rgba(0, 0, 0, 0.1) for better contrast.
    
    These changes enhance the application's branding by clearly displaying the logo and brand name in the header. The visual updates improve the UI's consistency and aesthetic appeal, ensuring that branding elements are prominent and well-integrated across the layout.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,11 @@
 import { HeaderTypes } from '@_types/components/layout';
+import { Brand } from '@components/shared';
 import { HeaderStyles } from '@styles/layout';
 
 export default function Header({}: HeaderTypes.Props) {
-    return <header className={HeaderStyles.Header}></header>;
+    return (
+        <header className={HeaderStyles.Header}>
+            <Brand />
+        </header>
+    );
 }
--- src/components/shared/Brand.tsx
+++ src/components/shared/Brand.tsx
@@ -1,6 +1,12 @@
 import { BrandTypes } from '@_types/components/shared';
 import { BrandStyles } from '@styles/shared';
+import { Logo } from '@components/shared';
 
 export default function Brand({}: BrandTypes.Props) {
-    return <div className={BrandStyles.Brand}></div>;
+    return (
+        <div className={BrandStyles.Brand}>
+            <Logo />
+            <p>Volty</p>
+        </div>
+    );
 }
