49d92760b56863ba3537199456601628a3293025
sr-githaka <sr.githaka@gmail.com>
2025-03-07 07:25:48 +0300

    feat(nav-aside): implement toggleable nav menu with useTrigger hook
    Modified the NavAside component to integrate toggle functionality by importing and utilizing the useTrigger hook. Added the 'use client' directive, updated the icon to conditionally display either a menu or cancel icon based on the trigger state, and rendered the navigation body only when triggered.
    
    This change enhances user interaction by allowing dynamic control of the navigation panel, thereby improving the user experience and optimizing UI performance.

--- src/components/shared/NavAside.tsx
+++ src/components/shared/NavAside.tsx
@@ -1,19 +1,25 @@
+'use client';
 import { NavAsideTypes } from '@_types/components/shared';
 import { NavAsideStyles } from '@styles/shared';
 import { Icon } from '@components/shared';
+import { useTrigger } from '@lib/hooks';
 
 export default function NavAside({ children }: NavAsideTypes.Props) {
+    const { trigger, pullTrigger } = useTrigger();
     return (
         <aside className={NavAsideStyles.NavAside}>
-            <div className={NavAsideStyles.Head}>
+            <div
+                className={NavAsideStyles.Head}
+                onClick={pullTrigger}
+            >
                 <Icon
-                    name='menu.svg'
+                    name={trigger ? 'cancel.svg' : 'menu.svg'}
                     alt='Menu icon'
                     width={24}
                     height={24}
                 />
             </div>
-            <div className={NavAsideStyles.Body}>{children}</div>
+            {trigger && <div className={NavAsideStyles.Body}>{children}</div>}
         </aside>
     );
 }
