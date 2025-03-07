1bdd6813eafd75156a0ae64fd68ead483b28c624
sr-githaka <sr.githaka@gmail.com>
2025-03-07 17:25:26 +0300

    feat(menu): implement toggleable menu overlay with expanded view
    Integrated the useTrigger hook into the Menu component to manage toggle state, switching between open and close icons based on user interaction. Updated the component to conditionally render an overlay (Body) that includes a Container for menu content and a Filler area that dismisses the overlay when clicked. Modified Menu.module.css to add styles for the overlay, container, and filler, including absolute positioning, padding, z-index, and responsive design enhancements.
    
    This change improves navigation by providing a dynamic, interactive menu that enhances user experience with clear visual feedback and an accessible overlay for additional menu options.

--- src/components/shared/Menu.tsx
+++ src/components/shared/Menu.tsx
@@ -1,18 +1,34 @@
+'use client';
 import { MenuTypes } from '@_types/components/shared';
 import { MenuStyles } from '@styles/shared';
-import { Icon } from '@components/shared';
+import { Icon, Logo } from '@components/shared';
+import { useTrigger } from '@lib/hooks';
+import { ocrA } from '@fonts/static';
 
 export default function Menu({}: MenuTypes.Props): MenuTypes.Return {
+    const { trigger, pullTrigger } = useTrigger();
     return (
         <div className={MenuStyles.Menu}>
-            <div className={MenuStyles.Head}>
+            <div
+                className={MenuStyles.Head}
+                onClick={pullTrigger}
+            >
                 <Icon
-                    name='close.svg'
+                    name={trigger ? 'open.svg' : 'close.svg'}
                     alt='Menu icon'
                     width={12}
                     height={12}
                 />
             </div>
+            {trigger && (
+                <div className={MenuStyles.Body}>
+                    <div className={MenuStyles.Container}></div>
+                    <div
+                        className={MenuStyles.Filler}
+                        onClick={pullTrigger}
+                    ></div>
+                </div>
+            )}
         </div>
     );
 }
